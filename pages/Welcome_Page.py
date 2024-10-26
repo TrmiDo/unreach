207b27d347bbaec0b00d943b8639621fb7dd86e0
DianaDancea <82067628+DianaDancea@users.noreply.github.com>
2024-10-26 01:50:57 +0000

    updated pages

--- pages/Welcome_Page.py
+++ pages/Welcome_Page.py
@@ -3,3 +3,5 @@ import streamlit as st
 
 st.title("Welcome Page")
 st.write("This is who we are..... and this is what we're trying to do...")
+
+st.write("Use the sidebar to navigate to different pages.")
