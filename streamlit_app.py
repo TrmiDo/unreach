33ddbb6a5e36686ae8606c2a8ab03f912ff781ef
DianaDancea <82067628+DianaDancea@users.noreply.github.com>
2024-10-26 01:52:27 +0000

    updated home page

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.title("Welcome to My Multi-Page Streamlit App")
+st.title("Home Page")
 st.write("We could have a verse here like:  \" 19 Therefore go and make disciples of all nations, baptizing them in the name of the Father and of the Son and of the Holy Spirit, 20 and teaching them to obey everything I have commanded you. And surely I am with you always, to the very end of the age.\" ")
 st.write("Matthew 28:19-20")
